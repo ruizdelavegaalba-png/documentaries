c9ff1cc778a15463362774165a0c1b79983b7b0c
ruizdelavegaalba <ruizdelavegaalba@gmail.com>
2026-04-28 12:05:38 +0200

    adding main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const requestURL = '../json/documentaries.json';
+
+async function fetchMoviesJson() {
+    const response = await fetch(requestURL);
+    const movies = await response.json();
+    return movies;
+}
+
+fetchMoviesJson().then(movies => {
+    for (let index = 0; index < movies.documentaries.length; index++) {
+        const moviesSection = document.getElementById('movieSection');
+
+        let id = movies.documentaries[index].id;
+        let poster = movies.documentaries[index].poster;
+        let title = movies.documentaries[index].title;
+        let year = movies.documentaries[index].year;
+        let length = movies.documentaries[index].length;
+        let director = movies.documentaries[index].director;
+        let synopsis = movies.documentaries[index].synopsis;
+
+        moviesSection.innerHTML += `
+        <div class="card" style="width: 18rem;">
+        <img src="${poster}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text"><span class="h6">${year}</span> . ${length}</p>
+                <h6 class="card-title mb-4">${director}</h6>
+                <p class="card-text">${synopsis}</p>
+            </div>
+        </div>
+        `
+    }
+})
